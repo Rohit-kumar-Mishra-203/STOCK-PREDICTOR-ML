f9ada39ddfab1ad8f8e663fb3e9f2be5fc22b74d
Rohit-kumar-mishra-203 <mishrarohitkumar2003@gmail.com>
2026-08-13 21:06:11 +0530

    dockerised everything

--- src/dashboard/app.py
+++ src/dashboard/app.py
@@ -17,8 +17,10 @@ import requests
 import pandas as pd
 import streamlit as st
 import plotly.graph_objects as go
+import os
 
-API_BASE_URL = "http://127.0.0.1:8000"
+
+API_BASE_URL = os.getenv("API_BASE_URL", "http://127.0.0.1:8000")
 
 st.set_page_config(page_title="Stock Movement Predictor", layout="wide")
 
